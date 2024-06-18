908b6aa509c44483ec38d5fefe0b1378263f9e5d
rosefinchh <nelsonritz55@gmail.com>
2024-06-18 11:52:29 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { Hono } from "hono";
+
+const app = new Hono();
+
+async function authMiddleware(c: any, next: any) {
+  if (c.req.header("Authorization")) {
+    // Do validation
+    await next();
+  } else {
+    return c.text("You don't have access");
+  }
+}
+
+app.use(authMiddleware);
+
+app.get("/", async (c) => {
+  return c.text("Hi this is a hono application");
+});
+
+app.post("/", async (c) => {
+  const body = await c.req.json();
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.header("userName"));
+  console.log(c.req.query("n"));
+
+  return c.text("Hello hono and wrangler and user!");
+});
+
+export default app;
